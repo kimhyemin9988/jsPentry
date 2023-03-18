8cf7e963dfe4b858ac504e921f092abac9d9e14f
kimhyemin9988 <rlagpals159@naver.com>
2023-03-18 12:52:50 +0900

    1

--- js/mobile.js
+++ js/mobile.js
@@ -1,20 +1,17 @@
+let draggedElement;
+let prevBox;
+let nextBox;
+
 dragula([document.getElementById("food"),
 document.getElementById("frozen"),
 document.getElementById("refrigerated"),
 document.getElementById("roomTemp")], {
     moves: function (el, source, handle, sibling) {
-        //console.log(el); // listBox
-        //console.log(source); // temp-box
-        //console.log(handle); // 클릭한 것
-        return true; // elements are always draggable by default
+        draggedElement = el;
+        prevBox = source.id;
+        return true;
     },
-    accepts: function (el, target, source, sibling) {
-        console.log(el); 
-        console.log(source); 
-        console.log(target); 
-        console.log(sibling);
-        return true; // elements can be dropped in any of the `containers` by default
-      },
-
-});
-//alertKey
+}).on("drop", function(el) {
+    nextBox = el.closest(".temp-box").id;
+    alertKey(draggedElement,prevBox,nextBox);
+})
