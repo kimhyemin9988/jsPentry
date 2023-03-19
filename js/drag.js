5996ba7c1a9f60ec661772f9abfd5b46ce3767f0
kimhyemin9988 <rlagpals159@naver.com>
2023-03-19 16:49:17 +0900

    1

--- js/drag.js
+++ js/drag.js
@@ -1,19 +1,14 @@
 
 let dragged = null;
-let foodDrag = document.getElementById("food");
-//감시대상 node
+let foodDrag = document.getElementById("food"); //감시대상 node
 let listBox = [];
-const targetTempBox = document.querySelectorAll(".temp-box");
-//드래그할것을 놓을 타겟을 모두
+const targetTempBox = document.querySelectorAll(".temp-box"); //드래그할것을 놓을 타겟을 모두
 const DRAGGING = "dragging";
 
-//드래그 스타트를 하면 그 이벤트의 타겟이 dragged이다
 
-//인스턴스
 let observer = new MutationObserver(() => {
   // 노드가 변경 됐을 때의 작업
-  refresh();
-  //노드가 변경되었을때 다시 불러오기
+  refresh(); //노드가 변경되었을때 다시 불러오기
 })
 // html에 그려지는 리스트의 수가 변하면
 let option = {
@@ -50,22 +45,20 @@ function opacReset() {
 };
 
 
-
 /* 빈배열을 두지 않으면 null됨 */
 let emptyArray = [];
 
 /* dragged 움직이는 html 요소 */
 function alertKey(dragged, prevBoxKeyName, nextBoxKeyName) {
-  const savedFood = JSON.parse(localStorage.getItem(prevBoxKeyName));
-  //JSON.parse(dragged.firstChild.id) 지울것 아이디
-  emptyArray = savedFood.filter((i) => i.id !== JSON.parse(dragged.firstChild.id)); // 지울것 지우고 나머지 filter
-  localStorage.setItem(prevBoxKeyName, JSON.stringify(emptyArray)); // update
-
-
-  const tempStoredFood = savedFood.filter((i) => i.id === JSON.parse(dragged.firstChild.id)); // 다른 key에 저장할 것
-  if (tempStoredFood.length !== 0) {
-    return saveTempBox(nextBoxKeyName, tempStoredFood);
-  }
+  const savedFood = getAndParse(prevBoxKeyName);
+  emptyArray = savedFood.filter((i) => i.id !== parseInt(dragged.firstChild.id)); // 지울것 지우고 나머지 filter
+  saveFood(emptyArray, prevBoxKeyName);// update
+  const tempStoredFood = savedFood.filter((i) => i.id === parseInt(dragged.firstChild.id)); // 다른 key에 저장할 것
+  /*
+    if (tempStoredFood.length !== 0) {}
+  
+  */
+  return saveTempBox(nextBoxKeyName, tempStoredFood);
 };
 
 
@@ -88,36 +81,14 @@ function endDrag(element) {
 
 function refresh() {
   listBox = document.querySelectorAll(".listBox");
-  /* mobile touch 
-  listBox.forEach((element) => {
-    element.addEventListener("touchstart", () => handleStart(element));
-    element.addEventListener("touchend", () => handleEnd(element));
-    log("Initialized.");
-  });*/
-  /* web touch */
+  /* web Drag
+  놓을 타겟에 이벤트 드래그 오버
+  drop한곳이 가능한 자리면 이전 자리에 있는 것을 지우고
+  drop event target child로 추가
+  */
   listBox.forEach((element) => startDrag(element));
   document.addEventListener("dragend", opacReset);
   targetTempBox.forEach((element) => endDrag(element));
-  //4.놓을 타겟에 이벤트 드래그 오버를 실행시키고
-  //그 이벤트의 타겟의 기본동작을 막는다
-  //5.타겟(놓을자리)의 이벤트 drop이 발생하면 그 이벤트의 기본을 막는다
-  //6.그 이벤트의 클래스네임이 드롭존이라면(놓을 수 있는 자리라면)
-  //7.지금 드래그중인것의 부모의 자식(본인)을 지운다
-  //8.그리고 이벤트의 타겟(본인)의 자리에 자식을 추가한다
 }
-/* 진행 중인 터치를 추적 
-const ongoingTouches = [];
-document.addEventListener("DOMContentLoaded", refresh);
 
-*/
 refresh();
-
-/*
-    drag and drop 모바일 구현
-    var el = document.getElementById('drag');
-  el.addEventListener("touchstart", handleStart, false);
-  el.addEventListener("touchend", handleEnd, false);
-  el.addEventListener("touchcancel", handleCancel, false);
-  el.addEventListener("touchleave", handleEnd, false);
-  el.addEventListener("touchmove", handleMove, false);
-*/
