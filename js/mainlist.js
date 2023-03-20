cfbab0f52a2b3a2ed52485d04825dddafb679a81
kimhyemin9988 <rlagpals159@naver.com>
2023-03-20 19:50:49 +0900

    1

--- js/mainlist.js
+++ js/mainlist.js
@@ -40,7 +40,7 @@ const getAndParse = (i) => {
 };
 
 const body = document.querySelector("body");
-let storedFood= [];
+let storedFood = [];
 
 /* modal open btn */
 const entireLiBtn = document.querySelectorAll(".entire-li-btn");
@@ -72,7 +72,7 @@ function inputFood(event) {
     price: foodPValue,
     exDate: foodEXValue,
   };
-  storedFood = savedFood ? savedFood : [];
+  storedFood = localStorage.getItem('food') !== null ? JSON.parse(localStorage.getItem('food'))  : [];
   storedFood.push(newFoodobj);
   saveFood(storedFood, "food"); //localStorage에 저장
   addList(newFoodobj);
@@ -115,8 +115,8 @@ function CreateSVG(button) {
   svgElem.style.display = "block";
   var coords =
     "M135.2 17.7L128 32H32C14.3 32 0 46.3 0 64S14.3 96 32 96H416c17.7 0 32-14.3 32-32s-14.3-32-32-32H320l-7.2-14.3C307.4 6.8 296.3 0 284.2 0H163.8c-12.1 0-23.2 6.8-28.6 17.7zM416 128H32L53.2 467c1.6 25.3 22.6 45 47.9 45H346.9c25.3 0 46.3-19.7 47.9-45L416 128z";
-    var path = document.createElementNS(xmlns, "path");
-    path.setAttributeNS(null, 'd', coords);
+  var path = document.createElementNS(xmlns, "path");
+  path.setAttributeNS(null, 'd', coords);
   svgElem.appendChild(path);
   button.appendChild(svgElem);
 }
